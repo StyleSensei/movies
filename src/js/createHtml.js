4cb06b4090ee958b0a648e49df398b3265408b5e
Patrik Arell <patrikarell@gmail.com>
2024-02-03 00:17:57 +0100

    ♻️ refractoring

--- src/js/createHtml.js
+++ src/js/createHtml.js
@@ -1,188 +1,135 @@
+import { trailers } from './trailers';
+
 export function createHtml(movies) {
-    const title = document.createElement("h1");
-    const headerElement = document.createElement('header')
-    const movieWrapper = document.createElement("section");
-    const movieContainer = document.createElement("section");
-    const trailers = [
-      "https://www.dailymotion.com/embed/video/x1daqie?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x1v36kg?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x864f6k?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x7ys69h?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x8hri2i?autoplay=1",
-      "https://www.dailymotion.com/embed/video/xh5m6v?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x7twi0g?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x8i0iyq?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x81kbh6?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x72ym6l?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x82d1ta?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x8ks1kh?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x8nct2q?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x7xcuxb?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x2hqukw?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x7z5yw9?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x387nfx?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x81ktm7?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x8p2t3b?autoplay=1",
-      "https://www.dailymotion.com/embed/video/xv4tkw?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x82ltpn?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x8hzd9w?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x4z57hw?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x3f9esi?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x7zdwz4?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x15tgvi?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x8hspgj?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x7u5rhn?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x8nqwwy?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x8m3rlp?autoplay=1",
-      "https://www.dailymotion.com/embed/video/xnc1lu?autoplay=1",
-      "https://www.dailymotion.com/embed/video/x59y8h8?autoplay=1",
-    ];
+  const title = document.createElement('h1');
+  const headerElement = document.createElement('header');
+  const movieWrapper = document.createElement('section');
+  const movieContainer = document.createElement('section');
 
-    movieContainer.classList.add("movieContainer");
-    movieWrapper.classList.add("movieWrapper");
-    headerElement.classList.add('header')
-    title.innerHTML = "Movies";
-    document.body.appendChild(headerElement);
-    headerElement.appendChild(title)
-    document.body.appendChild(movieWrapper);
-    movieWrapper.appendChild(movieContainer);
+  movieContainer.classList.add('movieContainer');
+  movieWrapper.classList.add('movieWrapper');
+  headerElement.classList.add('header');
+  title.innerHTML = 'Movies';
+  document.body.appendChild(headerElement);
+  headerElement.appendChild(title);
+  document.body.appendChild(movieWrapper);
+  movieWrapper.appendChild(movieContainer);
 
-     
-setTimeout(() => {
-  
-  headerElement.classList.add('--hide')
-}, 3000);      
-      
-    
-
+  const createCards = () => {
     for (let i = 0; i < movies.length; i++) {
-      // const myMovies = movies[i];
-      const movieTitle = document.createElement("p");
-      const movieYear = document.createElement("p");
-      const buttonContainer = document.createElement("div");
-      const streamNowBtn = document.createElement("button");
-      const trailerBtn = document.createElement("button");
-      const movieDescriptionContainer = document.createElement("article");
-      const movieDescription = document.createElement("p");
-      const movieDescriptionShowMore = document.createElement("a");
-      const moviePrice = document.createElement("p");
-      const card = document.createElement("section");
-      const movieImageContainer = document.createElement("image");
-      const movieImage = document.createElement("img");
-      const trailer = document.createElement("iframe");
-      const closeBtn = document.createElement("button");
-      const closeBtnImg = document.createElement("img");
-      const playBtn = document.createElement("button");
-      const playBtnImg = document.createElement("img");
+      const movieTitle = document.createElement('p');
+      const movieYear = document.createElement('p');
+      const buttonContainer = document.createElement('div');
+      const streamNowBtn = document.createElement('button');
+      const trailerBtn = document.createElement('button');
+      const movieDescriptionContainer = document.createElement('article');
+      const movieDescription = document.createElement('p');
+      const movieDescriptionShowMore = document.createElement('a');
+      const moviePrice = document.createElement('p');
+      const card = document.createElement('section');
+      const movieImageContainer = document.createElement('image');
+      const movieImage = document.createElement('img');
+      const trailer = document.createElement('iframe');
+      const closeBtn = document.createElement('button');
+      const closeBtnImg = document.createElement('img');
+      const playBtn = document.createElement('button');
+      const playBtnImg = document.createElement('img');
 
-      card.classList.add("card");
-      card.id = "card" + (i + 1);
-      movieTitle.classList.add("movieTitle");
-      movieYear.classList.add("movieYear");
-      buttonContainer.classList.add("buttonContainer");
-      streamNowBtn.classList.add("streamNowBtn");
-      trailerBtn.classList.add("trailerBtn");
-      moviePrice.classList.add("moviePrice");
-      movieDescriptionContainer.classList.add("movieDescription__container");
-      movieDescription.classList.add("movieDescription");
-      movieDescriptionShowMore.classList.add("movieDescription__showmore");
-      movieImageContainer.classList.add("movieImageContainer");
-      movieImage.classList.add("movieImage");
-      movieImage.setAttribute("src", movies[i].imageUrl);
-      trailer.setAttribute("src", trailers[i]);
-      trailer.classList.add("trailer");
+      card.id = 'card' + (i + 1);
+      card.classList.add('card');
+      movieTitle.classList.add('movieTitle');
+      movieYear.classList.add('movieYear');
+      buttonContainer.classList.add('buttonContainer');
+      streamNowBtn.classList.add('streamNowBtn');
+      trailerBtn.classList.add('trailerBtn');
+      moviePrice.classList.add('moviePrice');
+      movieDescriptionContainer.classList.add('movieDescription__container');
+      movieDescription.classList.add('movieDescription');
+      movieDescriptionShowMore.classList.add('movieDescription__showmore');
+      movieImageContainer.classList.add('movieImageContainer');
+      movieImage.classList.add('movieImage');
+      playBtnImg.classList.add('playBtnImg');
+      playBtn.classList.add('playBtn');
+      trailer.classList.add('trailer');
+      closeBtn.classList.add('closeBtn');
+      closeBtnImg.classList.add('closeBtnImg');
+
+      movieImage.setAttribute('src', movies[i].imageUrl);
+      trailer.setAttribute('src', trailers[i]);
       trailer.controls = true;
-      closeBtnImg.setAttribute("src", "./assets/media/close-svgrepo-com.svg");
-      closeBtnImg.classList.add("closeBtnImg");
-      closeBtn.classList.add("closeBtn");
+      closeBtnImg.setAttribute('src', './assets/media/close-svgrepo-com.svg');
       playBtnImg.setAttribute(
-        "src",
-        "./assets/media/play_circle_FILL0_wght400_GRAD0_opsz24.svg"
+        'src',
+        './assets/media/play_circle_FILL0_wght400_GRAD0_opsz24.svg'
       );
-      playBtnImg.classList.add("playBtnImg");
-      playBtn.classList.add("playBtn");
 
       movieTitle.innerHTML = movies[i].name;
       movieYear.innerHTML = movies[i].year;
-      streamNowBtn.innerHTML = "Stream now";
-      trailerBtn.innerHTML = "Play trailer";
+      streamNowBtn.innerHTML = 'Stream now';
+      trailerBtn.innerHTML = 'Play trailer';
       movieDescription.innerHTML = movies[i].description;
-      movieDescriptionShowMore.innerHTML = "Show more";
-      moviePrice.innerHTML = movies[i].price + "kr";
+      movieDescriptionShowMore.innerHTML = 'Show more';
+      moviePrice.innerHTML = movies[i].price + 'kr';
 
-      movieDescriptionShowMore.addEventListener("click", (e) => {
-        e.target.classList.toggle("showFullDescription");
+      movieDescriptionShowMore.addEventListener('click', (e) => {
+        e.target.classList.toggle('showFullDescription');
         movieDescriptionContainer.classList.toggle(
-          "movieDescription__container--showfull"
+          'movieDescription__container--showfull'
         );
-        movieDescription.classList.toggle("movieDescription--showfull");
-        if (e.target.classList.contains("showFullDescription")) {
-          e.target.innerHTML = "Show less";
+        movieDescription.classList.toggle('movieDescription--showfull');
+        if (e.target.classList.contains('showFullDescription')) {
+          e.target.innerHTML = 'Show less';
         } else {
-          e.target.innerHTML = "Show more";
+          e.target.innerHTML = 'Show more';
         }
       });
 
-
-      card.addEventListener("mouseenter", () => {
-        if (!movieImageContainer.classList.contains("trailer")) {
-          playBtn.classList.add("visible");
+      card.addEventListener('mouseenter', () => {
+        if (!movieImageContainer.classList.contains('trailer')) {
+          playBtn.classList.add('visible');
         }
       });
-      card.addEventListener("mouseleave", () => {
-        playBtn.classList.remove("visible");
+      card.addEventListener('mouseleave', () => {
+        playBtn.classList.remove('visible');
       });
 
-      playBtn.addEventListener("click", () => {
-        movieImageContainer.classList.add("trailer");
-        movieImage.classList.add("movieImage__hide");
-        movieImage.classList.toggle("expand");
-        movieImageContainer.classList.toggle("expand");
-        card.classList.toggle("expand");
-        trailer.classList.toggle("expand");
+      const showTrailer = () => {
+        movieImageContainer.classList.add('trailer');
+        movieImage.classList.add('movieImage__hide');
+        movieImage.classList.toggle('expand');
+        movieImageContainer.classList.toggle('expand');
+        card.classList.toggle('expand');
+        trailer.classList.toggle('expand');
         card.appendChild(closeBtn);
         closeBtn.appendChild(closeBtnImg);
         movieImageContainer.appendChild(trailer);
-        playBtn.classList.remove("visible");
+        playBtn.classList.remove('visible');
+      };
+
+      playBtn.addEventListener('click', () => {
+        showTrailer();
       });
 
-      movieImageContainer.addEventListener("click", () => {
-        movieImageContainer.classList.add("trailer");
-        movieImage.classList.add("movieImage__hide");
-        movieImage.classList.toggle("expand");
-        movieImageContainer.classList.toggle("expand");
-        card.classList.toggle("expand");
-        trailer.classList.toggle("expand");
-        card.appendChild(closeBtn);
-        closeBtn.appendChild(closeBtnImg);
-        movieImageContainer.appendChild(trailer);
+      movieImageContainer.addEventListener('click', () => {
+        showTrailer();
       });
 
-      trailerBtn.addEventListener("click", () => {
-        // movieImageContainer.classList.add("card__mobileHover")
-        movieImageContainer.classList.add("trailer");
-        movieImage.classList.add("movieImage__hide");
-        movieImage.classList.toggle("expand");
-        movieImageContainer.classList.toggle("expand");
-        card.classList.toggle("expand");
-        trailer.classList.toggle("expand");
-        card.appendChild(closeBtn);
-        closeBtn.appendChild(closeBtnImg);
-        movieImageContainer.appendChild(trailer);
+      trailerBtn.addEventListener('click', () => {
+        showTrailer();
       });
 
-      closeBtn.addEventListener("click", () => {
+      closeBtn.addEventListener('click', () => {
         // movieImageContainer.classList.remove("card__mobileHover")
-        movieImageContainer.classList.remove("trailer");
-        movieImage.classList.remove("movieImage__hide");
+        movieImageContainer.classList.remove('trailer');
+        movieImage.classList.remove('movieImage__hide');
         movieImageContainer.removeChild(trailer);
-        movieImage.classList.remove("movieImage__hide");
-        movieImage.classList.toggle("expand");
-        movieImageContainer.classList.toggle("expand");
-        card.classList.toggle("expand");
-        trailer.classList.toggle("expand");
+        movieImage.classList.remove('movieImage__hide');
+        movieImage.classList.toggle('expand');
+        movieImageContainer.classList.toggle('expand');
+        card.classList.toggle('expand');
+        trailer.classList.toggle('expand');
         card.removeChild(closeBtn);
-        playBtn.classList.remove("visible");
+        playBtn.classList.remove('visible');
       });
 
       movieContainer.appendChild(card);
@@ -200,5 +147,7 @@ setTimeout(() => {
       card.appendChild(playBtn);
       playBtn.appendChild(playBtnImg);
     }
+  };
+  createCards();
+}
 
-  }
--- src/js/services/movieService.js
+++ src/js/services/movieService.js
@@ -1,10 +1,16 @@
-import { createHtml } from "../createHtml";
-
 export async function logMovies() {
-    const response = await fetch(
-      "https://medieinstitutet-wie-products.azurewebsites.net/api/products"
-    );
-    const movies = await response.json();
-  
-    createHtml(movies);
-  }
+  const response = await fetch(
+    'https://medieinstitutet-wie-products.azurewebsites.net/api/products'
+  );
+  const movies = await response.json();
+  movies.forEach(movie => {
+    if(movie.name === "Star Wars"){
+      movie.imageUrl = "https://se.originalfilmart.com/cdn/shop/products/star_wars_1977_style_c_printers_proof_original_film_art_5000x.webp?v=1680027227"
+    }
+    if(movie.name === "Alien") {
+      movie.imageUrl = "https://se.originalfilmart.com/cdn/shop/products/alien_1979_german_a1_original_film_art_5000x.jpg?v=1613719562"
+    }
+  });
+  console.log(movies)
+  return movies;
+}
