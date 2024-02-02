fd56ae23985b42339013273e204a786aa65dd3d1
Patrik Arell <patrikarell@gmail.com>
2024-02-02 13:34:22 +0100

    update deploy method

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
+// @ts-ignore
+import * as VitePluginCopy from 'vite-plugin-copy';
 
-// https://vite.js.dev/config
+// https://vitejs.dev/config/
 export default defineConfig({
-    base: "/movies/"
-})
+  base: "/movies/",
+  plugins: [
+    // @ts-ignore
+    VitePluginCopy.copy({
+      patterns: [
+        { src: 'src/media/*', dest: 'dist/media' },
+      ],
+    }),
+  ],
+});
